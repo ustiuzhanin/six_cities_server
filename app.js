1e6f647c7ed13e66252020109d5498c1598b756a
Slava <ucheba00@gmail.com>
2020-11-07 14:47:54 -0700

    add graphql

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const { graphqlHTTP } = require("express-graphql");
+
+const graphqlSchema = require("./graphql/schema");
+const graphqlResolver = require("./graphql/resolvers");
+
+const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.l4nq1.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true&w=majority`;
 
 const app = express();
 
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver,
+    graphiql: true,
+  })
+);
+
+app.use("/", (req, res, next) => {
+  console.log("qqqq");
+});
+
 mongoose
-  .connect("mongo")
+  .connect(MONGODB_URI)
   .then((result) => {
-    app.listen(8080);
+    app.listen(process.env.PORT || 3000);
   })
   .catch((err) => console.log(err));
